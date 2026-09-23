ff60a43a3671875488c2e5e2c2c52ffe32383636
Emmy <89320276+emmyfong@users.noreply.github.com>
2026-09-23 10:32:02 -0700

    Added simulator loop to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,20 @@
-#include "OrderBook.h"
-#include "CSVParser.h"
+#include "Simulator.h"
 #include <iostream>
-#include <chrono>
 
 //Helper vars
 uint64_t currentTimestep = 1;
 uint64_t getNextTime() { return currentTimestep++; }
 
 int main() {
-    std::cout << "[1] Parsing CSV into memory...\n";
+    std::cout << "=========================================\n";
+    std::cout << "   HIGH-FREQUENCY LOB SIMULATOR (v1.0)   \n";
+    std::cout << "=========================================\n";
 
-    //load orders into ram
-    std::vector<Order> orders = CSVParser::parse("data/orders.csv");
+    //instantiate engine with data file
+    Simulator engine("data/orders.csv");
 
-    if (orders.empty()) {
-        std::cerr << "ERROR: No orders loaded. Check if the data/orders.csv exists\n";
-        return 1;
-    }
+    //Run multithreaded simulation
+    engine.run();
 
-    std::cout << "Loaded " << orders.size() << " orders\n";
-    std::cout << "[2] Starting Matching Engine...\n";
-
-    OrderBook lob;
-
-    //Start clock
-    auto start = std::chrono::high_resolution_clock::now();
-
-    //sim loop
-    for (const auto& order : orders) {
-        lob.addOrder(order);
-    }
-
-    auto end = std::chrono::high_resolution_clock::now();
-
-    //calculate elapsed time in ms
-    std::chrono::duration<double, std::micro> elapsed = end - start;
-
-    //throughput -> orders per sec
-    double seconds = elapsed.count() / 1'000'000.0;
-    double ops = orders.size() / seconds;
-
-    std::cout << "\n========== PERFORMANCE METRICS ==========\n";
-    std::cout << "Time elapsed: " << elapsed.count() << " microseconds\n";
-    std::cout << "Throughput:   " << ops << " orders / second\n";
-    std::cout << "=========================================\n";    
-
-    //lob.printBook()
-
-    return 0;
+    return 0;    
 }
